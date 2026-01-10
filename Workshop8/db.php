06370676f809fc8b2b62b37f985ed6e346621fcb
Pooja <bhandari.pooja2061@gmail.com>
2026-01-10 15:20:35 +0545

    workshop8

---
+++ Workshop8/db.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$database = 'school_db';
+$port = 3306;
+
+$conn = mysqli_connect($host, $user, $password, $database, $port);
+
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+}
+
+?>
